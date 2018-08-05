2d37a5883a2a54dcae61e1f123fe8587f8b37717
koketani <ykoketani@gmail.com>
2018-08-05 16:45:44 +0900

    pcap tool; add tcp stream parser

--- pcap/main.go
+++ pcap/main.go
@@ -3,12 +3,54 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 	// "strings"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
+
+type TCPID struct {
+	clientPort layers.TCPPort
+	serverPort layers.TCPPort
+}
+
+type TCPSegment struct {
+	timestamp time.Time
+	tcp       layers.TCP
+}
+
+type TCPStream struct {
+	id       TCPID
+	segments []TCPSegment
+}
+
+func NewTCPStream(tcpID TCPID) TCPStream {
+	return TCPStream{tcpID, make([]TCPSegment, 100)}
+}
+
+func getDirectional(srcPort, destPort layers.TCPPort) (clientPort, serverPort layers.TCPPort) {
+	if srcPort < destPort {
+		clientPort = destPort
+		serverPort = srcPort
+	} else {
+		clientPort = srcPort
+		serverPort = destPort
+	}
+	return
+}
+
+func obtainTCPLayer(packet gopacket.Packet) (*layers.TCP, error) {
+	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+		if tcp, ok := tcpLayer.(*layers.TCP); ok {
+			return tcp, nil
+		} else {
+			return nil, fmt.Errorf("")
+		}
+	}
+	return nil, fmt.Errorf("")
+}
 
 func main() {
 	handle, err := pcap.OpenOffline(os.Args[1])
@@ -18,16 +60,26 @@ func main() {
 		os.Exit(1)
 	}
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	fmt.Println(handle.LinkType())
-	for packet := range packetSource.Packets() {
-		handlePacket(packet) // Do something with a packet here.
+	packets := packetSource.Packets()
+	var tcpStreams = make(map[TCPID]*TCPStream, len(packets))
+	for packet := range packets {
+		tcp, err := obtainTCPLayer(packet)
+		if err != nil {
+			fmt.Printf("failed read %s", packet)
+			os.Exit(1)
+		}
+		timestamp := packet.Metadata().Timestamp.UTC()
+		clientPort, serverPort := getDirectional(tcp.SrcPort, tcp.DstPort)
+		tcpID := TCPID{clientPort, serverPort}
+		if stream, ok := tcpStreams[tcpID]; ok {
+			(*stream).segments = append((*stream).segments, TCPSegment{timestamp, *tcp})
+		} else {
+			newstream := NewTCPStream(tcpID)
+			newstream.segments = append(newstream.segments, TCPSegment{timestamp, *tcp})
+			tcpStreams[tcpID] = &newstream
+		}
 	}
-}
-
-func handlePacket(packet gopacket.Packet) {
-	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-		tcp, _ := tcpLayer.(*layers.TCP)
-		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
-		fmt.Printf("%b, %b, %b\n", tcp.SYN, tcp.ACK, tcp.FIN)
+	for id, _ := range tcpStreams {
+		fmt.Println(id)
 	}
 }
