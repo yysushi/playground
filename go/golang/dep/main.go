86c2b5c72b4e727facb359a5e10ed54c1c148aa9
koketani <ykoketani@gmail.com>
2018-09-01 12:04:56 +0900

    add an example of golang/dep to checck dep ensure behavior

--- go/golang/dep/main.go
+++ go/golang/dep/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type Person struct {
+	Type string
+	Name string
+}
 
 func main() {
-	fmt.Println("vim-go")
+	var result Person
+	input := map[string]string{
+		"type": "person",
+		"name": "koke",
+	}
+	err := mapstructure.Decode(input, &result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	fmt.Println(reflect.TypeOf(result))
 }
