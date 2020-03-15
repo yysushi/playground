c21e876afddee30929e521a6a5e33bb8a0619755
koketani <ykoketani@gmail.com>
2020-03-15 14:47:52 +0900

    rename for sub test and add skip testing

--- go/golang/testing/skip_test.go
+++ go/golang/testing/skip_test.go
@@ -4,16 +4,8 @@ import (
 	"testing"
 )
 
-func TestFoo(t *testing.T) {
-	// <setup code>
-	t.Run("A=1", func(t *testing.T) {
-		t.Log("A=1")
-	})
-	t.Run("A=2", func(t *testing.T) {
-		t.Log("A=2")
-	})
-	t.Run("B=1", func(t *testing.T) {
-		t.Log("B=1")
-	})
-	// <tear-down code>
+func TestTimeConsuming(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
 }
