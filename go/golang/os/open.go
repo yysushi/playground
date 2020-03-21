d0bf8fdd290a5dcee4486a08d7fe543a18024fa7
koketani <ykoketani@gmail.com>
2020-03-21 10:15:11 +0900

    wip

--- go/golang/os/open.go
+++ go/golang/os/open.go
@@ -3,23 +3,40 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	// func os.Open(name string) (*os.File, error)
-	f, err := os.Open("hoge.txt")
+	// func os.Create(name string) (*os.File, error)
+	wf, err := os.Create("hoge.txt")
 	if err != nil {
-		return
+		panic(err)
+	}
+	w := bufio.NewWriter(wf)
+	// func fmt.Fprintln(w io.Writer, a ...interface{}) (n int, err error)
+	fmt.Fprintln(w, "hello\nworld")
+	w.WriteString("hello world again\n")
+	wf.Close()
+	// func bufio.NewReader(rd io.Reader) *bufio.Reader
+	// os.File implements interface type io.Reader
+	// func os.Open(name string) (*os.File, error)
+	rf, err := os.Open("hoge.txt")
+	if err != nil {
+		panic(err)
 	}
 	// func bufio.NewReader(rd io.Reader) *bufio.Reader
 	// os.File implements interface type io.Reader
-	r := bufio.NewReader(f)
+	r := bufio.NewReader(rf)
 	for {
 		// func (*bufio.Reader).ReadLine() (line []byte, isPrefix bool, err error)
 		line, _, err := r.ReadLine()
 		if err != nil {
-			return
+			if err != io.EOF {
+				panic(err)
+			}
+			fmt.Println(string(line))
+			break
 		}
 		fmt.Println(string(line))
 	}
