2c25019676fa7ec1f7b4bea98c82e2dbea25a79d
koketani <ykoketani@gmail.com>
2020-03-21 15:42:41 +0900

    update examples of golang uber fx

--- go/go.uber.org/fx/main.go
+++ go/go.uber.org/fx/main.go
@@ -2,21 +2,54 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
 	"net/http"
 
 	"go.uber.org/fx"
 )
 
-func register(lifecycle fx.Lifecycle) {
-	mux := http.NewServeMux()
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+// Config ...
+type Config struct {
+	Addr string
+}
+
+// NewConfig ...
+func NewConfig() *Config {
+	configFileName := flag.String("config", "config.sample.json", "")
+	flag.Parse()
+	b, err := ioutil.ReadFile(*configFileName)
+	if err != nil {
+		panic(err)
 	}
+	var config Config
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		panic(err)
+	}
+	return &config
+}
+
+// NewServeMux ...
+func NewServeMux() *http.ServeMux {
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+	return mux
+}
 
+// NewServer ...
+func NewServer(mux *http.ServeMux, config *Config) *http.Server {
+	return &http.Server{
+		Addr:    config.Addr,
+		Handler: mux,
+	}
+}
+
+// Register ...
+func Register(lifecycle fx.Lifecycle, server *http.Server) {
 	lifecycle.Append(
 		fx.Hook{
 			OnStart: func(context.Context) error {
@@ -32,6 +65,9 @@ func register(lifecycle fx.Lifecycle) {
 
 func main() {
 	fx.New(
-		fx.Invoke(register),
+		fx.Provide(NewConfig),
+		fx.Provide(NewServer),
+		fx.Provide(NewServeMux),
+		fx.Invoke(Register),
 	).Run()
 }
