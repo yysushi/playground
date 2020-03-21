3772e187793d44ee4e12f172445bb13511100bd3
koketani <ykoketani@gmail.com>
2020-03-21 11:29:14 +0900

    update golang testing package samples

---
+++ go/golang/testing/advanced/sub_test.go
@@ -0,0 +1,40 @@
+package sub_test
+
+import (
+	"testing"
+)
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+func setupSubTest(t *testing.T) func(t *testing.T) {
+	t.Log("setup")
+	return func(t *testing.T) {
+		t.Log("teardown")
+	}
+}
+
+// https://stackoverflow.com/questions/42310088/setup-and-teardown-for-each-test-using-std-testing-package/42310257#42310257
+func TestAdvancedSub(t *testing.T) {
+	cases := []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	}{
+		{"basic", 1, 2, 3},
+		{"minus", 1, -2, -1},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			teardownSubTest := setupSubTest(t)
+			defer teardownSubTest(t)
+
+			result := Sum(tc.a, tc.b)
+			if result != tc.expected {
+				t.Fatalf("expected sum %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
