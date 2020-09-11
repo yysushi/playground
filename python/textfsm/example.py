8cd1196e22ef601684a9f4bc26757ec59c154d7c
koketani <ykoketani@gmail.com>
2020-09-11 21:14:16 +0900

    update python/textfsm

--- python/textfsm/example.py
+++ python/textfsm/example.py
@@ -1,7 +1,8 @@
 import sys
 
 import textfsm
-# Run text through the FSM. 
+
+# Run text through the FSM.
 # The argument 'template' is a file handle and 'raw_text_data' is a string.
 with open(sys.argv[1]) as template:
     re_table = textfsm.TextFSM(template)
@@ -11,7 +12,7 @@ data = re_table.ParseText(raw_text_data)
 
 # Display result as CSV
 # First the column headers
-print( ', '.join(re_table.header) )
+print(", ".join(re_table.header))
 # Each row of the table.
 for row in data:
-  print( ', '.join(row) )
+    print(", ".join(row))
