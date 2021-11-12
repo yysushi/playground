5198f3938c3b5673866f224f9c05780dc8b0551c
hanaugai <ykoketani@gmail.com>
2021-11-12 07:25:25 -0800

    golang icmp; ping loop

--- go/golang/x/net/icmp/main.go
+++ go/golang/x/net/icmp/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"os"
+	"os/signal"
 	"runtime"
+	"sync"
+	"time"
 
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
@@ -26,42 +30,74 @@ func main() {
 		log.Fatal("test only in darwin environment")
 	}
 
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	c, err := icmp.ListenPacket("udp4", outboundIP())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer c.Close()
 
-	wm := icmp.Message{
-		Type: ipv4.ICMPTypeEcho, Code: 0,
-		Body: &icmp.Echo{
-			ID: os.Getpid() & 0xffff, Seq: 1,
-			Data: []byte("HELLO-R-U-THERE"),
-		},
-	}
-	wb, err := wm.Marshal(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP("8.8.8.8"), Zone: "en0"}); err != nil {
-		log.Fatal(err)
-	}
+	var wg sync.WaitGroup
 
-	rb := make([]byte, 1500)
-	n, peer, err := c.ReadFrom(rb)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rm, err := icmp.ParseMessage(1, rb[:n])
-	if err != nil {
-		log.Fatal(err)
-	}
-	switch rm.Type {
-	case ipv4.ICMPTypeEchoReply:
-		m, _ := rm.Body.Marshal(1)
-		log.Println(string(m))
-		log.Printf("got reflection from %v", peer)
-	default:
-		log.Printf("got %+v; want echo reply", rm)
-	}
+	// sender
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-time.After(time.Second):
+				wm := icmp.Message{
+					Type: ipv4.ICMPTypeEcho, Code: 0,
+					Body: &icmp.Echo{
+						ID: os.Getpid() & 0xffff, Seq: 1,
+						Data: []byte("HELLO-R-U-THERE"),
+					},
+				}
+				wb, err := wm.Marshal(nil)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP("8.8.8.8"), Zone: "en0"}); err != nil {
+					log.Fatal(err)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	// receiver
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+
+			rb := make([]byte, 1500)
+			n, peer, err := c.ReadFrom(rb)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rm, err := icmp.ParseMessage(1, rb[:n])
+			if err != nil {
+				log.Fatal(err)
+			}
+			switch rm.Type {
+			case ipv4.ICMPTypeEchoReply:
+				m, _ := rm.Body.Marshal(1)
+				log.Println(string(m))
+				log.Printf("got reflection from %v", peer)
+			default:
+				log.Printf("got %+v; want echo reply", rm)
+			}
+		}
+	}()
+
+	wg.Wait()
 }
