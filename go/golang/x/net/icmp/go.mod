142667317adf2142afe5c16535b006bbc2f93f53
hanaugai <ykoketani@gmail.com>
2021-11-11 18:55:53 -0800

    add icmp sample

---
+++ go/golang/x/net/icmp/go.mod
@@ -0,0 +1,7 @@
+module hoge
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20211111160137-58aab5ef257a
+
+require golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
---
+++ go/golang/x/net/icmp/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"runtime"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
+func outboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
+
+func main() {
+	switch runtime.GOOS {
+	case "darwin":
+	default:
+		log.Fatal("test only in darwin environment")
+	}
+
+	c, err := icmp.ListenPacket("udp4", outboundIP())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte("HELLO-R-U-THERE"),
+		},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP("8.8.8.8"), Zone: "en0"}); err != nil {
+		log.Fatal(err)
+	}
+
+	rb := make([]byte, 1500)
+	n, peer, err := c.ReadFrom(rb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rm, err := icmp.ParseMessage(1, rb[:n])
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch rm.Type {
+	case ipv4.ICMPTypeEchoReply:
+		log.Printf("got reflection from %v", peer)
+	default:
+		log.Printf("got %+v; want echo reply", rm)
+	}
+}
