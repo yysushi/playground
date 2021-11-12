0ed64128530fecd493e855889c813ac7cad98527
hanaugai <ykoketani@gmail.com>
2021-11-12 09:50:13 -0800

    golang icmp; embed message

--- go/golang/x/net/icmp/main.go
+++ go/golang/x/net/icmp/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"errors"
 	"log"
 	"net"
 	"os"
@@ -23,6 +26,52 @@ func outboundIP() string {
 	return conn.LocalAddr().(*net.UDPAddr).IP.String()
 }
 
+type customMessage struct {
+	pid    int
+	sentAt time.Time
+}
+
+func newCustomMessage(pid int) *customMessage {
+	return &customMessage{
+		pid:    pid,
+		sentAt: time.Now(),
+	}
+}
+
+type EmbeddedMessage struct {
+	PID    int64
+	SentAt int64
+}
+
+func (c *customMessage) toEmbed() *EmbeddedMessage {
+	return &EmbeddedMessage{
+		PID:    int64(c.pid), // fixed size is necessary
+		SentAt: c.sentAt.UnixMicro(),
+	}
+}
+
+func newCustomMessageFromBinary(s []byte, expectPID int) (*customMessage, error) {
+	buf := bytes.NewReader(s)
+	var em EmbeddedMessage
+	err := binary.Read(buf, binary.LittleEndian, &em)
+	if err != nil {
+		return nil, err
+	}
+	if int(em.PID) != expectPID {
+		return nil, errors.New("other sender")
+	}
+	return &customMessage{
+		pid:    int(em.PID),
+		sentAt: time.UnixMicro(em.SentAt),
+	}, nil
+}
+
+func (c *customMessage) toBytes() []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, c.toEmbed())
+	return buf.Bytes()
+}
+
 func main() {
 	switch runtime.GOOS {
 	case "darwin":
@@ -40,6 +89,7 @@ func main() {
 	defer c.Close()
 
 	var wg sync.WaitGroup
+	pid := os.Getpid()
 
 	// sender
 	wg.Add(1)
@@ -48,11 +98,12 @@ func main() {
 		for {
 			select {
 			case <-time.After(time.Second):
+				custom := newCustomMessage(pid)
 				wm := icmp.Message{
 					Type: ipv4.ICMPTypeEcho, Code: 0,
 					Body: &icmp.Echo{
-						ID: os.Getpid() & 0xffff, Seq: 1,
-						Data: []byte("HELLO-R-U-THERE"),
+						ID: pid & 0xffff, Seq: 1,
+						Data: custom.toBytes(),
 					},
 				}
 				wb, err := wm.Marshal(nil)
@@ -91,7 +142,11 @@ func main() {
 			switch rm.Type {
 			case ipv4.ICMPTypeEchoReply:
 				m := (rm.Body).(*icmp.Echo)
-				log.Printf("got reflection from %v with (id:%d, seq:%d, data:%s)", peer, m.ID, m.Seq, m.Data)
+				if c, err := newCustomMessageFromBinary(m.Data, pid); err != nil {
+					log.Printf("got reflection from %v, but ignore with %s", peer, err)
+				} else {
+					log.Printf("got reflection from %v with (id:%d, seq:%d, data:%#v)", peer, m.ID, m.Seq, c)
+				}
 			default:
 				log.Printf("got %+v; want echo reply", rm)
 			}
