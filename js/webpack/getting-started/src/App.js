0c27621c5deed1f810ea07b15e9880e21cc873f6
hanaugai <ykoketani@gmail.com>
2022-01-02 18:43:12 -0800

    js; in webpack, use function coomponent

--- js/webpack/getting-started/src/App.js
+++ js/webpack/getting-started/src/App.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import React from 'react';
 
-const App = () => (
-  <h1>{_.join(['Hello', 'webpack'], ' ')}</h1>
-)
+function App() {
+  return <h1>{_.join(['Hello', 'webpack'], ' ')}</h1>;
+}
 
 export default App
