b84f39f88a610e35fd1c7b68af7acdf0d9a58f9a
hanaugai <ykoketani@gmail.com>
2022-01-02 18:35:57 -0800

    js; webpack integration with babel/react

--- js/webpack/getting-started/webpack.config.js
+++ js/webpack/getting-started/webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
   },
 };
