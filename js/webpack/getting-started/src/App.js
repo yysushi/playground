93811515822a2b84d4f6c48aef39b2d6703f31c2
hanaugai <ykoketani@gmail.com>
2022-01-02 18:35:57 -0800

    js; webpack integration with babel/react

---
+++ js/webpack/getting-started/src/App.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+import React from 'react';
+
+const App = () => (
+  <h1>{_.join(['Hello', 'webpack'], ' ')}</h1>
+)
+
+export default App
--- js/webpack/getting-started/src/index.js
+++ js/webpack/getting-started/src/index.js
@@ -1,12 +1,5 @@
-import _ from 'lodash';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(<App />, document.getElementById('app'))
