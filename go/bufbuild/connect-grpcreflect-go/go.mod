570db0c2b4ae94b620a4ad85a3fce72b94d9d070
yysushi <yoshihiro.tsuji2@gmail.com>
2023-07-15 21:32:39 +0900

    add go/bufbuild/connect-grpcreflect-go

---
+++ go/bufbuild/connect-grpcreflect-go/go.mod
@@ -0,0 +1,14 @@
+module github.com/yysushi/playground/go/bufbuild/connect-grpcreflect-go
+
+go 1.20
+
+require (
+	github.com/bufbuild/connect-grpcreflect-go v1.1.0
+	golang.org/x/net v0.12.0
+)
+
+require (
+	github.com/bufbuild/connect-go v1.7.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ go/bufbuild/connect-grpcreflect-go/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	reflector := grpcreflect.NewStaticReflector(
+		"acme.user.v1.UserService",
+		"acme.group.v1.GroupService",
+		// protoc-gen-connect-go generates package-level constants
+		// for these fully-qualified protobuf service names, so you'd more likely
+		// reference userv1.UserServiceName and groupv1.GroupServiceName.
+	)
+	mux.Handle(grpcreflect.NewHandlerV1(reflector))
+	// Many tools still expect the older version of the server reflection API, so
+	// most servers should mount both handlers.
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(reflector))
+	// If you don't need to support HTTP/2 without TLS (h2c), you can drop
+	// x/net/http2 and use http.ListenAndServeTLS instead.
+	http.ListenAndServe(
+		":8080",
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
