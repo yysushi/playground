3ff3a137ebcd9fcf315ae48ec3ecdd0f7fee76b5
yysushi <yoshihiro.tsuji2@gmail.com>
2024-05-25 20:58:33 +0900

    option b

--- go/generics/main_test.go
+++ go/generics/main_test.go
@@ -13,39 +13,31 @@ import (
 // create network <- create & network impl
 // create network <- create & network impl
 
-type Validator interface {
-	Validate(string) bool
+type Logic struct {
+	validate func(string) bool
 }
 
-type Provider[T Validator] struct {
-	// NOTE: embedded field type cannot be a (pointer to a) type parameter (typecheck)
-	// T
-	validator T
+type Provider struct {
+	Logic
 }
 
-// check "known limitations" in https://tip.golang.org/doc/go1.18#generics
-// and this https://github.com/golang/go/issues/49030
-func (p Provider[T]) Validate(input string) bool {
-	return p.validator.Validate(input)
-}
-
-func (p Provider[T]) Create(input string) bool {
-	if !p.Validate(input) {
+func (p Provider) Create(input string) bool {
+	if !p.validate(input) {
 		return false
 	}
 	// some processings...
 	return true
 }
 
-type NetworkResource struct{}
-
-func (r NetworkResource) Validate(input string) bool {
+func networkValidate(input string) bool {
 	return len(input) < 5
 }
 
 func TestA(t *testing.T) {
-	p := &Provider[NetworkResource]{
-		validator: NetworkResource{},
+	p := &Provider{
+		Logic{
+			validate: networkValidate,
+		},
 	}
 	if p.Create("12345") {
 		t.Fail()
