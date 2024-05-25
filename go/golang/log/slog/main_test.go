7e1bf44c52ef906ac96462ed7a2e2de793040f62
yysushi <yoshihiro.tsuji2@gmail.com>
2024-05-25 20:58:38 +0900

    fix log

--- go/golang/log/slog/main_test.go
+++ go/golang/log/slog/main_test.go
@@ -3,11 +3,22 @@ package main_test
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+	logsv1 "go.opentelemetry.io/proto/otlp/logs/v1"
 )
+
+var traceKey = logsv1.File_opentelemetry_proto_logs_v1_logs_proto.Messages().ByName("LogRecord").Fields().ByNumber(9).Name()
+var spanKey = logsv1.File_opentelemetry_proto_logs_v1_logs_proto.Messages().ByName("LogRecord").Fields().ByNumber(10).Name()
 
 func removeTime(groups []string, a slog.Attr) slog.Attr {
 	if a.Key == slog.TimeKey && len(groups) == 0 {
@@ -92,4 +103,29 @@ func TestB(t *testing.T) {
 	logger = logger.WithGroup("g4").With("k4", "v4")
 	logger.InfoContext(ctx, "msg4")
 	assert.Equal(t, "level=INFO msg=msg4 k1=v1 g2.k2=v2 g2.k3=v3 g2.g4.k4=v4 trace_id=trace_value\n", buf.String())
+}
+
+func TestC(t *testing.T) {
+	// writer
+	f, err := os.Create("traces.txt")
+	require.NoError(t, err)
+	defer f.Close()
+	// exporter
+	exporter, err := stdouttrace.New(
+		stdouttrace.WithWriter(f),
+	)
+	require.NoError(t, err)
+	// trace provider
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(exporter))
+	otel.SetTracerProvider(tp)
+
+	tracer1 := otel.Tracer("tracer1")
+	ctx1, span1 := tracer1.Start(context.Background(), "span1", trace.WithNewRoot())
+	defer span1.End()
+	ctx2, span2 := tracer1.Start(ctx1, "span2")
+	defer span2.End()
+	fmt.Println(trace.SpanContextFromContext(ctx1).SpanID())
+	fmt.Println(trace.SpanContextFromContext(ctx1).TraceID())
+	fmt.Println(trace.SpanContextFromContext(ctx2).SpanID())
+	fmt.Println(trace.SpanContextFromContext(ctx2).TraceID())
 }
