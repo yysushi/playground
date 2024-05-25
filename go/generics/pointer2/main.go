92cff16d711db39b816de1cd37cd3d3a3dda3cbb
yysushi <yoshihiro.tsuji2@gmail.com>
2024-05-25 20:58:33 +0900

    wip

---
+++ go/generics/pointer2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type myEntry struct{}
+
+func (e *myEntry) String() string {
+	return "entry"
+}
+
+type myType interface {
+	String() string
+}
+
+type Server[T1 myType, T2 []T1] struct {
+	t T2
+}
+
+func (s *Server[T1, T2]) Hoge(T1) {
+}
+
+type myTypedServer = Server[myEntry, []myEntry]
+
+func main() {
+	s := &myTypedServer{
+		t: []myEntry{1},
+	}
+	fmt.Printf("%#v", s)
+	fmt.Println()
+}
