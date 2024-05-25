4daa62a2d7e9249cba2b3c87f18004c5f999933e
yysushi <yoshihiro.tsuji2@gmail.com>
2024-05-25 20:58:33 +0900

    wip

---
+++ go/generics/pointer/main.go
@@ -0,0 +1,62 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+type Person struct {
+	ID   int
+	Name string
+}
+
+type PersonDB interface {
+	save(person Person) error
+}
+
+type LocalDatabase struct{}
+
+type InMemoryDatabase struct {
+	m map[int]Person
+}
+
+func (db *LocalDatabase) save(person Person) error {
+	// Implementation for saving a person to the local database
+	fmt.Printf("Saving person in Local DB: %+v\n", person)
+
+	return nil
+}
+
+func (db *InMemoryDatabase) save(person Person) error {
+	// Implementation for saving a person to the map in meory
+	fmt.Printf("Saving person in memory DB: %+v\n", person)
+	mapd := db.m
+	if mapd == nil {
+		mapd = make(map[int]Person)
+	}
+	mapd[person.ID] = person
+	return nil
+}
+
+type DBType interface {
+	LocalDatabase | InMemoryDatabase
+}
+
+type DBPointer[T any] interface {
+	*T
+	PersonDB
+}
+
+func CreatePersonDB[T DBType, dbPointer DBPointer[T]](person Person) error {
+	var db dbPointer = new(T)
+	db.save(person)
+	return nil
+}
+
+func main() {
+	person := Person{
+		ID:   101,
+		Name: "John D",
+	}
+	CreatePersonDB[LocalDatabase](person)
+	CreatePersonDB[InMemoryDatabase](person)
+}
