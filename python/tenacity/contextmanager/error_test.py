ebf88b573e893d878eaa4f6522630cbfb3500152
yysushi <yoshihiro.tsuji2@gmail.com>
2025-10-18 12:11:59 -0700

    fixup! add another tenacity sample

--- python/tenacity/contextmanager/error_test.py
+++ python/tenacity/contextmanager/error_test.py
@@ -4,20 +4,21 @@ import typing as t
 import tenacity as tc
 
 
-class TargetException(Exception): ...
+class TargetException(BaseException): ...
 
 
-def target(error: str = ""):
+class OtherException(BaseException): ...
+
+
+def target(error: BaseException | None = None):
     if error:
-        raise TargetException(error)
-    else:
-        pass
+        raise error
 
 
 def attempts[T](
     max_attempt: int = 2,
     wait_fixed: float = 0.2,
-    error: type[T] | None = None,
+    error: type[T] | T | None = None,
 ) -> t.Iterator[t.ContextManager[None]]:
     def attempter(retry: tc.AttemptManager) -> t.ContextManager[None]:
         @contextlib.contextmanager
@@ -25,9 +26,21 @@ def attempts[T](
             with retry:
                 try:
                     yield
-                except Exception as exc:
-                    if error is None or not isinstance(exc, error):
-                        raise AssertionError("unexpected error")
+                except BaseException as exc:
+                    if (
+                        error is None
+                        or (isinstance(error, type) and not isinstance(exc, error))
+                        or (
+                            not isinstance(error, type)
+                            and not isinstance(exc, type(error))
+                        )
+                        or (
+                            not isinstance(error, type)
+                            and isinstance(exc, type(error))
+                            and str(error) not in str(exc)
+                        )
+                    ):
+                        raise AssertionError(f"unexpected error {exc}")
                 else:
                     if error:
                         raise AssertionError("unexpected success")
@@ -41,13 +54,51 @@ def attempts[T](
         yield attempter(retry)
 
 
-def test_expected_error():
+def test_expected_error_type():
     for attempt in attempts(error=TargetException):
         with attempt:
-            target("hoge")
+            target(TargetException("hoge"))
+
+
+def test_unexpected_error_type():
+    try:
+        for attempt in attempts(error=TargetException):
+            with attempt:
+                target(OtherException("hoge"))
+    except tc.RetryError:
+        pass
+    else:
+        raise AssertionError("succeeded unexpectedly")
+
+
+def test_expected_error_instance():
+    for attempt in attempts(error=TargetException("hoge")):
+        with attempt:
+            target(TargetException("hoge"))
+
+
+def test_unexpected_error_instance():
+    try:
+        for attempt in attempts(error=TargetException("hoge2")):
+            with attempt:
+                target(TargetException("hoge"))
+    except tc.RetryError:
+        pass
+    else:
+        raise AssertionError("succeeded unexpectedly")
 
 
 def test_expected_no_error():
     for attempt in attempts():
         with attempt:
             target()
+
+def test_unexpected_no_error():
+    try:
+        for attempt in attempts(error=TargetException("hoge")):
+            with attempt:
+                target()
+    except tc.RetryError:
+        pass
+    else:
+        raise AssertionError("succeeded unexpectedly")
