74403385c61d1e9745efe69338584e82cae28b23
yysushi <yoshihiro.tsuji2@gmail.com>
2025-10-18 16:00:18 -0700

    extend tenacity sample

--- python/tenacity/contextmanager/error_test.py
+++ python/tenacity/contextmanager/error_test.py
@@ -15,17 +15,24 @@ def target(error: BaseException | None = None):
         raise error
 
 
+class SuccessError(AssertionError): ...
+
+
+class FailureError(AssertionError): ...
+
+
 def attempts[T](
     max_attempt: int = 2,
     wait_fixed: float = 0.2,
     error: type[T] | T | None = None,
-) -> t.Iterator[t.ContextManager[None]]:
-    def attempter(retry: tc.AttemptManager) -> t.ContextManager[None]:
+    stop_error_type: type[SuccessError] | type[FailureError] | None = None,
+) -> t.Iterator[t.ContextManager[tc.RetryCallState]]:
+    def attempter(retry: tc.AttemptManager) -> t.ContextManager[tc.RetryCallState]:
         @contextlib.contextmanager
-        def asserter() -> t.Iterator[None]:
+        def asserter() -> t.Iterator[tc.RetryCallState]:
             with retry:
                 try:
-                    yield
+                    yield retry.retry_state
                 except BaseException as exc:
                     if (
                         error is None
@@ -40,15 +47,28 @@ def attempts[T](
                             and str(error) not in str(exc)
                         )
                     ):
-                        raise AssertionError(f"unexpected error {exc}")
+                        raise FailureError
                 else:
                     if error:
-                        raise AssertionError("unexpected success")
+                        raise SuccessError
 
         return asserter()
 
+    if stop_error_type:
+
+        class my_stop(tc.stop.stop_base):
+            def __call__(self, retry_state: tc.RetryCallState) -> bool:
+                if retry_state.outcome is None or not retry_state.outcome.failed:
+                    return False
+                return isinstance(retry_state.outcome.exception(), stop_error_type)
+
+        stop = tc.stop_any(tc.stop_after_attempt(max_attempt), my_stop())
+    else:
+        # retry = tc.retry_if_exception_type()  # default one
+        stop = tc.stop_after_attempt(max_attempt)
+
     for retry in tc.Retrying(
-        stop=tc.stop_after_attempt(max_attempt),
+        stop=stop,
         wait=tc.wait_fixed(wait_fixed),
     ):
         yield attempter(retry)
@@ -93,6 +113,7 @@ def test_expected_no_error():
         with attempt:
             target()
 
+
 def test_unexpected_no_error():
     try:
         for attempt in attempts(error=TargetException("hoge")):
@@ -100,5 +121,18 @@ def test_unexpected_no_error():
                 target()
     except tc.RetryError:
         pass
+    else:
+        raise AssertionError("succeeded unexpectedly")
+
+
+def test_unexpected_no_error_with_immediate_exit():
+    try:
+        for attempt in attempts(error=TargetException("hoge"), stop_error_type=SuccessError):
+            with attempt:
+                target()
+    except tc.RetryError as e:
+        assert e.last_attempt.failed
+        assert e.last_attempt.attempt_number == 1
+        assert isinstance(e.last_attempt.exception(), SuccessError)
     else:
         raise AssertionError("succeeded unexpectedly")
