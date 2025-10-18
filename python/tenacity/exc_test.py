e7dfcf2aab3680ac739d48fe6c8624e03c05b2f7
yysushi <yoshihiro.tsuji2@gmail.com>
2025-10-17 17:38:17 -0700

    fixup! init tenacity usage

--- python/tenacity/exc_test.py
+++ python/tenacity/exc_test.py
@@ -1,2 +1,14 @@
-def test_hoge():
-    print()
+import tenacity  as t
+
+class MyException(Exception): ...
+
+def raise_exc():
+    raise MyException()
+
+def no_raise_exc():
+    raise MyException()
+
+def test_raise_exc():
+    for attempt in t.Retrying():
+        with attempt:
+            raise_exc()
